5df785096c72df262c47c3ea84f8fb7c3c7541b5
Your Name <your_email@example.com>
2024-06-25 08:32:29 +0700

    FIFTH VERSION

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
-from flask import Flask, Response, jsonify, send_file, render_template
+from flask import Flask, Response, jsonify, send_file, render_template, request
 import cv2, os
 import threading
 import logging
 import subprocess
 import time
 import atexit
+import sqlite3
+from datetime import datetime
 from attendance_taker import Face_Recognizer
 
 app = Flask(__name__)
@@ -158,6 +160,30 @@ def index():
 @app.route("/index")
 def second_page():
     return render_template("index.html", selected_date="", no_data=False)
+
+
+@app.route("/attendance", methods=["POST"])
+def attendance():
+    selected_date = request.form.get("selected_date")
+    selected_date_obj = datetime.strptime(selected_date, "%Y-%m-%d")
+    formatted_date = selected_date_obj.strftime("%Y-%m-%d")
+
+    conn = sqlite3.connect("attendance.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT name, time FROM attendance WHERE date = ?", (formatted_date,)
+    )
+    attendance_data = cursor.fetchall()
+
+    conn.close()
+
+    if not attendance_data:
+        return render_template("index.html", selected_date=selected_date, no_data=True)
+
+    return render_template(
+        "index.html", selected_date=selected_date, attendance_data=attendance_data
+    )
 
 
 def generate_frames():
