8abba68bf85afc183f54b594eea9c2946338b6a1
Your Name <your_email@example.com>
2024-06-27 14:38:25 +0700

    SIXTH COMMIT

--- gui.py
+++ gui.py
@@ -1,4 +1,12 @@
-from flask import Flask, Response, jsonify, send_file, render_template, request
+from flask import (
+    Flask,
+    Response,
+    jsonify,
+    send_file,
+    render_template,
+    request,
+    make_response,
+)
 import cv2, os
 import threading
 import logging
@@ -7,6 +15,7 @@ import time
 import atexit
 import sqlite3
 from datetime import datetime
+import json
 from attendance_taker import Face_Recognizer
 
 app = Flask(__name__)
@@ -24,6 +33,8 @@ paused = False
 out = None  # -- it is used to store video file
 filename = None
 ffmpeg_process = None
+total_frames = 0
+frame_count = 0
 
 
 def gstreamer_pipeline(
@@ -52,6 +63,68 @@ def gstreamer_pipeline(
 
 output_dir = "videos/"
 os.makedirs(output_dir, exist_ok=True)
+
+
+class VideoDurationFetcher:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+        self.video_durations = {}
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.fetch_all_video_durations()
+        self.start_periodic_refresh()
+
+    def fetch_all_video_durations(self):
+        self.video_durations = {}
+        for filename in os.listdir(self.output_dir):
+            if filename.endswith(".mp4"):
+                video_path = os.path.join(self.output_dir, filename)
+                duration = self.get_video_duration_from_file(video_path)
+                if duration is not None:
+                    self.video_durations[filename] = duration
+
+    def get_video_duration_from_file(self, video_path):
+        command = [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "json",
+            video_path,
+        ]
+        try:
+            result = subprocess.run(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            if result.returncode == 0:
+                duration_info = json.loads(result.stdout)
+                duration = float(duration_info["format"]["duration"])
+                return duration
+            else:
+                error_message = result.stderr.decode("utf-8").strip()
+                print(f"Failed to get duration for {video_path}: {error_message}")
+                return None
+        except Exception as e:
+            print(
+                f"Exception occurred while getting duration for {video_path}: {str(e)}"
+            )
+            return None
+
+    def start_periodic_refresh(self):
+        def periodic_refresh():
+            while True:
+                self.fetch_all_video_durations()
+                time.sleep(60)
+
+        thread = threading.Thread(target=periodic_refresh, daemon=True)
+        # daemon = True: class run independtly and does not block main program
+        thread.start()
+
+
+# -- end of class VideoDurationFetcher
+# Create instance of VideoDurationFetcher
+video_duration_fetcher = VideoDurationFetcher(output_dir)
 
 
 # this function is used for writing video into file with h265 codec
@@ -162,6 +235,8 @@ def second_page():
     return render_template("index.html", selected_date="", no_data=False)
 
 
+# -----------------------------------------------------------------------------------------
+# this function is used for index.html
 @app.route("/attendance", methods=["POST"])
 def attendance():
     selected_date = request.form.get("selected_date")
@@ -184,6 +259,9 @@ def attendance():
     return render_template(
         "index.html", selected_date=selected_date, attendance_data=attendance_data
     )
+
+
+# ---------------------------------------------------------------------------------------
 
 
 def generate_frames():
@@ -292,12 +370,6 @@ def stop_camera():
         return jsonify(status="Error stopping camera", error=str(e)), 500
 
 
-"""@app.route("/showAttendance", methods=["POST"])
-def show_attendance():
-    print("SHOW ATTENDANCE BUTTON IS CLICKED")
-"""
-
-
 @app.route("/register", methods=["POST"])
 def register():
     print("REGISTER BUTTON IS CLICKED")
@@ -311,6 +383,8 @@ def register():
 @app.route("/view_saved", methods=["GET"])
 def view_saved():
     print("VIEW BUTTON IS CLICKED")
+    global frame_count
+    frame_count = 0
     try:
         saved_videos = [f for f in os.listdir(output_dir) if f.endswith(".mp4")]
         return jsonify(status="Success", saved_videos=saved_videos)
@@ -321,11 +395,14 @@ def view_saved():
 
 # display video onto gui from video file
 def generate_frames_from_file(filename):
+    global total_frames, frame_count
     try:
+        frame_count = 0
         cap = cv2.VideoCapture(os.path.join(output_dir, filename))
         if not cap.isOpened():
             logging.error("Failed to open video file.")
             return
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
         while cap.isOpened():
             global paused
@@ -333,7 +410,6 @@ def generate_frames_from_file(filename):
             while paused:
                 # Sleep to reduce CPU usage when paused
                 time.sleep(1)
-
             ret, frame = cap.read()
             if not ret:
                 break
@@ -349,8 +425,10 @@ def generate_frames_from_file(filename):
                     b"--frame\r\n"
                     b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n"
                 )
+                frame_count += 1
             else:
                 logging.error("Failed to encode frame.")
+
             time.sleep(0.03)
 
         cap.release()
@@ -358,27 +436,66 @@ def generate_frames_from_file(filename):
         logging.error(f"Error generating frames from file: {e}")
 
 
-@app.route("/videos/<filename>")
-def get_video(filename):
-    return send_file(os.path.join(output_dir, filename))
-
-
 @app.route("/play_video/<filename>")
 def play_video(filename):
-    global paused
+    global paused, frame_count, total_frames
+    total_frames = 0
+    frame_count = 0
     if paused:
         paused = False  # Resume playback
+
     return Response(
         generate_frames_from_file(filename),
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
 
 
+@app.route("/slider_update", methods=["GET"])
+def slider_update():
+    global frame_count, total_frames
+    print("frame_total: ", total_frames)
+    print("frame count: ", frame_count)
+    # Calculate the progress as a percentage
+    if total_frames > 0 and frame_count != 0:
+        progress = frame_count / total_frames
+    else:
+        progress = 0
+
+    # Create the JSON response with the correct Content-Type header
+    response = make_response(jsonify({"status": "success", "progress": progress}))
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    return response
+
+
 @app.route("/toggle_pause", methods=["POST"])
 def toggle_pause():
     global paused
     paused = not paused  # Toggle pause state
-    return "Paused" if paused else "Resumed"
+    return "▶️" if paused else "||"
+
+
+@app.route("/get_video_duration", methods=["GET"])
+def get_video_duration():
+    filename = request.args.get("filename")  # Get filename from query parameters
+
+    if filename in video_duration_fetcher.video_durations:
+        return jsonify(
+            {
+                "filename": filename,
+                "duration": video_duration_fetcher.video_durations[filename],
+            }
+        )
+    else:
+        video_path = os.path.join(output_dir, filename)
+        if not os.path.isfile(video_path):
+            return jsonify({"error": f"Video file not found: {video_path}"}), 404
+
+        duration = video_duration_fetcher.get_video_duration_from_file(video_path)
+        if duration is not None:
+            video_duration_fetcher.video_durations[filename] = duration
+            return jsonify({"filename": filename, "duration": duration})
+        else:
+            return jsonify({"error": f"Failed to get duration for {filename}"}), 500
 
 
 # Graceful shutdown function to release resources
